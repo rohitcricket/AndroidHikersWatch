e6129efb49770495be02af626b5598b6d1645851
ROHIT GUPTA <rohitgupta@ROHITs-MacBook-Pro-2.local>
2017-04-29 22:18:33 -0700

    added location info

--- app/src/main/java/com/example/android/hikerswatch/MainActivity.java
+++ app/src/main/java/com/example/android/hikerswatch/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.android.hikerswatch;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -14,6 +16,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -46,12 +52,52 @@ public class MainActivity extends AppCompatActivity {
         TextView longTextView = (TextView) findViewById(R.id.longTextView);
         TextView accTextView = (TextView) findViewById(R.id.accTextView);
         TextView altTextView = (TextView) findViewById(R.id.altTextView);
-        TextView addressTextView = (TextView) findViewById(R.id.addressTextView);
 
         latTextView.setText("Latitude: " + location.getLatitude());
         longTextView.setText("Longitude: " + location.getLongitude());
         altTextView.setText("Altitude: " + location.getAltitude());
         accTextView.setText("Accuracy: " + location.getAccuracy());
+
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+        try {
+
+            String address = "Could not find address";
+
+            List<Address> listAddresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+
+            if (listAddresses != null && listAddresses.size() > 0) {
+
+                Log.i("Placesinfo", listAddresses.get(0).toString());
+
+                address = "";
+
+                if (listAddresses.get(0).getSubThoroughfare() != null) {
+                    address += listAddresses.get(0).getSubThoroughfare() + " ";
+                }
+
+                if (listAddresses.get(0).getThoroughfare() != null) {
+                    address += listAddresses.get(0).getThoroughfare() + "\n";
+                }
+
+                if (listAddresses.get(0).getLocality() != null) {
+                    address += listAddresses.get(0).getLocality() + "\n";
+                }
+
+                if (listAddresses.get(0).getPostalCode() != null) {
+                    address += listAddresses.get(0).getPostalCode() + "\n";
+                }
+
+            }
+
+            TextView addressTextView = (TextView) findViewById(R.id.addressTextView);
+
+            addressTextView.setText(address);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
